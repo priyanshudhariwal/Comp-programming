aee8b0aa29c47dada607f2c904849e72d5b41e71
Priyanshu Dhariwal <priyanshudhariwal@gmail.com>
2021-12-24 01:54:53 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,18 @@
+/*
+	raven_28
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	//solve here
+	return 0;
+}
